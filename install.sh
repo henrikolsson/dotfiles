30d600c6e546ea4c66124f914400489e1b20ded6
Henrik Olsson <henrik@fixme.se>
2013-11-14 11:56:48 +0100

    Add override

--- install.sh
+++ install.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+REL="$(lsb_release -sc)"
 cd "$DIR"
-find . -type f -not -path "./.git/*" -not -path "./install.sh" | while read file
+find . -type f -not -path "./.git/*" -not -path "./install.sh" -not -path "./override/*" | while read file
 do
     d=$(dirname "$file")
     if [ ! -d "$HOME/$d" ]
     then
         mkdir -pv "$HOME/$d"
     fi
+    if [ -e "$DIR/override/$file.$REL" ]
+    then
+        target="/override/$file.$REL"
+    else
+        target="$file"
+    fi
     if [ -e "$HOME/$file" ]
     then
-        diff -uw "$HOME/$file" "$DIR/$file"
+        diff -uw "$HOME/$file" "$DIR/$target"
         if [ $? -eq 1 ]
         then
             rm -v "$HOME/$file"
-            ln -vs "$DIR/$file" "$HOME/$file"
+            ln -vs "$DIR/$target" "$HOME/$file"
         fi
     else
-        ln -vs "$DIR/$file" "$HOME/$file"
+        ln -vs "$DIR/$target" "$HOME/$file"
     fi
 done
